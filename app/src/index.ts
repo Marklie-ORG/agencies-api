d884fa6d8c19d5ea938fa5f7115d71ceea942a01
Roman Pchelintsev <74241615+RomaPchel@users.noreply.github.com>
2025-03-06 11:10:24 +0200

    Initial commit

---
+++ app/src/index.ts
@@ -0,0 +1,24 @@
+import Koa from "koa";
+import koabodyparser from "koa-bodyparser";
+import {HelloController} from "./lib/controllers/HelloController";
+import {orm} from "./lib/db/config/DB";
+import {ErrorMiddleware} from "./lib/middlewares/ErrorMiddleware";
+import {ValidationMiddleware} from "./lib/middlewares/ValidationMiddleware";
+
+const app = new Koa();
+
+await orm.connect().then(() => {
+  console.log("Database has connected!");
+});
+
+app.use(koabodyparser());
+app.use(ErrorMiddleware());
+app.use(ValidationMiddleware());
+app.use(koabodyparser());
+app
+    .use(new HelloController().routes())
+    .use(new HelloController().allowedMethods());
+
+app.listen(3000, () => {
+  console.log(`Auth server is running at ${3000}`);
+});
