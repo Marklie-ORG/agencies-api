4941c0628a76519e4ef893882afea390d499d2a3
romapchel <notpchelintsevroma@gmail.com>
2025-03-26 16:13:52 +0200

    update main entities to keep the dev branch updated

--- app/src/lib/entities/OrganizationMember.ts
+++ app/src/lib/entities/OrganizationMember.ts
@@ -2,7 +2,7 @@ import { Entity, ManyToOne, Property } from "@mikro-orm/core";
 import { BaseEntity } from "./BaseEntity.js";
 import { User } from "./User.js";
 import { Organization } from "./Organization.js";
-import { OrganizationRole } from "../../enums/enums.js";
+import { OrganizationRole } from "../enums/enums.js";
 
 @Entity()
 export class OrganizationMember extends BaseEntity {
