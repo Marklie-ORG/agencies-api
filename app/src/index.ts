f0d2ac3729dcb3c14f68d37d205b1db41c4fc375
Andrii Derevianko <59490330+DereviankoAndrew@users.noreply.github.com>
2025-03-26 17:19:27 +0100

    ads controller

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,10 +1,12 @@
 import Koa from "koa";
 import koabodyparser from "koa-bodyparser";
+import cors from "@koa/cors";
 import { ValidationMiddleware } from "./lib/middlewares/ValidationMiddleware.js";
 import { ReportsController } from "./lib/controllers/ReportsController.js";
 import { ErrorMiddleware } from "./lib/middlewares/ErrorMiddleware.js";
 import { orm } from "./lib/db/config/DB.js";
 import { AuthController } from "./lib/controllers/AuthenticationController.js";
+import { AdAccountsController } from "lib/controllers/AdAccountsController.js";
 
 const app = new Koa();
 
@@ -12,6 +14,10 @@ await orm.connect().then(() => {
   console.log("Database has connected!");
 });
 
+app.use(cors({
+  origin: 'http://localhost:4200',
+  credentials: true
+}));
 app.use(koabodyparser());
 app.use(ErrorMiddleware());
 app.use(ValidationMiddleware());
@@ -21,8 +27,13 @@ app
   .use(new ReportsController().allowedMethods());
 app
   .use(new AuthController().routes())
-  .use(new AuthController().allowedMethods());
+  .use(new AuthController().allowedMethods())
 
+  .use(new AdAccountsController().routes())
+  .use(new AdAccountsController().allowedMethods());
+
+// app
+  
 app.listen(3000, () => {
   console.log(`Auth server is running at ${3000}`);
 });
